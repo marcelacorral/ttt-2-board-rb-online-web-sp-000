bded3c4f3bb139fbfa67e1f68062ee4221bdef6d
Marcela Corral <corralmarcela30@gmail.com>
2020-05-05 23:45:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,12 @@
 # Define the variable board below.
-board="board
-row = ["","",""]
+board="board"
+row = [" "," "," "]
 # Postion 1: Left 
 row [0]
 # Postion 2: Middle 
 row [1]
 # Postion 3: Right 
 row [2]
-# Move 
+# Move X to the Postion 2 , Middle 
+row [1] = "X" 
+row #=> [" " , "X" , " "] 
