30dcd8cf9bfa90744ae8d328dbc8ffbc1e45172e
Marcela Corral <corralmarcela30@gmail.com>
2020-05-06 00:18:51 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -13,6 +13,12 @@ row [0]
 row [1]
 # Position 6: Right 
 row [2]
+# Postion 7: Left 
+row [0]
+# Postion 8: Middle
+row [1]
+# Position 9: Right 
+row [2]
 
 # Move X to the Postion 2 , Middle 
 row [1] = "X" 
